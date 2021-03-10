981da1daad8dcb9c7ba17a55b5bd166cc60c0d3e
Ömer Biltekin <omerbiltekin_@outlook.com>
2021-03-11 00:06:35 +0300

    10 March end of lesson

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductResponseModel } from '../models/productResponseModel';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = 'https://localhost:44349/api/products/getall';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts():Observable<ProductResponseModel> {
+    return this.httpClient.get<ProductResponseModel>(this.apiUrl)
+      
+  }
+
+
+}
